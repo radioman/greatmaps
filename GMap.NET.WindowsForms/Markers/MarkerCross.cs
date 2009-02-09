91321170bf72cdbdeb3965171a82c421d567154e
radioman <none@none>
2009-02-09 19:58:13 +0000

    added MarkerGoogle.cs and MarkerCross.cs to GMap.NET.WindowsForms

---
+++ GMap.NET.WindowsForms/Markers/MarkerCross.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+
+namespace GMapNET
+{
+   public class MarkerCross : MapObject
+   {
+      public Pen Pen;
+
+      public MarkerCross()
+      {
+         Pen = new Pen(Brushes.Red, 1);
+      }
+
+      public void OnRender(Graphics g)
+      {
+         System.Drawing.Point p1 = new System.Drawing.Point(LocalPosition.X, LocalPosition.Y);
+         p1.Offset(0, -10);
+         System.Drawing.Point p2 = new System.Drawing.Point(LocalPosition.X, LocalPosition.Y);
+         p2.Offset(0, 10);
+
+         System.Drawing.Point p3 = new System.Drawing.Point(LocalPosition.X, LocalPosition.Y);
+         p3.Offset(-10, 0);
+         System.Drawing.Point p4 = new System.Drawing.Point(LocalPosition.X, LocalPosition.Y);
+         p4.Offset(10, 0);
+
+         g.DrawLine(Pen, p1, p2);
+         g.DrawLine(Pen, p3, p4);
+      }
+   }
+}
---
+++ GMap.NET.WindowsForms/Markers/MarkerGoogle.cs
@@ -0,0 +1,30 @@
+﻿using System.Drawing;
+using GMapNET.WindowsForms;
+
+namespace GMapNET
+{
+   public class MarkerGoogle : MapObject
+   {
+      public bool IsDragging;
+
+      public MarkerGoogle()
+      {
+         IsDragging = false;
+      }
+
+      public void OnRender(Graphics g)
+      {
+         if(!IsDragging)
+         {
+            g.DrawImageUnscaled(Resources.shadow50, LocalPosition.X-10, LocalPosition.Y-34);
+            g.DrawImageUnscaled(Resources.marker, LocalPosition.X-10, LocalPosition.Y-34);
+         }
+         else
+         {
+            g.DrawImageUnscaled(Resources.shadow50, LocalPosition.X-10, LocalPosition.Y-40);
+            g.DrawImageUnscaled(Resources.marker, LocalPosition.X-10, LocalPosition.Y-40);
+            g.DrawImageUnscaled(Resources.drag_cross_67_16, LocalPosition.X-8, LocalPosition.Y-8);
+         }
+      }
+   }
+}
