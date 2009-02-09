206efe3b0d327ad9f5a28483f5a194ca74621b25
radioman <none@none>
2009-02-09 07:16:29 +0000

    added MarkerDot.cs

---
+++ Demo.WindowsPresentation/Markers/MarkerDot.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using GMapNET;
+
+namespace Demo.WindowsPresentation
+{
+   public class MarkerDot : MapObject
+   {
+      public Pen Pen;
+      public double Radius = 5;
+
+      public MarkerDot()
+      {
+         Pen = new Pen(Brushes.Red, 2.0);
+      }
+
+      public void OnRender(DrawingContext g)
+      {
+         System.Windows.Point p = new System.Windows.Point(LocalPosition.X, LocalPosition.Y);
+         g.DrawEllipse(Brushes.Red, Pen, p, Radius, Radius);
+      }
+   }
+}
