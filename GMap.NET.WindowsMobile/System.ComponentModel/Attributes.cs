356740d3fd17b45e361b0c7b4e0f51b33efb4f6f
radioman <none@none>
2009-12-21 15:40:12 +0000

    added missing files

---
+++ GMap.NET.WindowsMobile/System.ComponentModel/Attributes.cs
@@ -0,0 +1,97 @@
+﻿
+namespace System.ComponentModel
+{
+   using System;
+
+   class CategoryAttribute : Attribute
+   {
+      string category;
+      public string Category
+      {
+         get
+         {
+            return category;
+         }
+         set
+         {
+            category = value;
+         }
+      }
+
+      public CategoryAttribute(string category)
+      {
+         this.Category = category;
+      }
+   }
+
+   class DescriptionAttribute : Attribute
+   {
+      string description;
+      public string Description
+      {
+         get
+         {
+            return description;
+         }
+         set
+         {
+            description = value;
+         }
+      }
+
+      public DescriptionAttribute(string description)
+      {
+         this.Description = description;
+      }
+   }
+
+   class BrowsableAttribute : Attribute
+   {
+      bool browsable;
+
+      public bool Browsable
+      {
+         get
+         {
+            return browsable;
+         }
+         set
+         {
+            browsable = value;
+         }
+      }
+
+      public BrowsableAttribute(bool browsable)
+      {
+         this.Browsable = browsable;
+      }
+   }
+
+   public enum DesignerSerializationVisibility
+   {
+      Hidden=0,
+      Visible=1,
+      Content=2,
+   }
+
+   public class DesignerSerializationVisibilityAttribute : Attribute
+   {
+      DesignerSerializationVisibility visibility;
+      public DesignerSerializationVisibility Visibility
+      {
+         get
+         {
+            return visibility;
+         }
+         set
+         {
+            visibility = value;
+         }
+      }
+
+      public DesignerSerializationVisibilityAttribute(DesignerSerializationVisibility visibility)
+      {
+         this.Visibility = visibility;
+      }
+   }
+}
