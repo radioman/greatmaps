a0108dd9d361f61e5cb595a943deb7212fe1e4df
radioman <none@none>
2010-02-18 20:18:53 +0200

    added missing CompositionTargetEx.cs

---
+++ GMap.NET.WindowsPresentation/GMap.NET.WindowsPresentation/CompositionTargetEx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Media;
+
+namespace GMap.NET.WindowsPresentation
+{
+   public static class CompositionTargetEx
+   {
+      static TimeSpan _last = TimeSpan.Zero;
+      static event EventHandler<RenderingEventArgs> _FrameUpdating;
+
+      public static event EventHandler<RenderingEventArgs> FrameUpdating
+      {
+         add
+         {
+            if(_FrameUpdating == null)
+               CompositionTarget.Rendering += CompositionTarget_Rendering;
+
+            _FrameUpdating += value;
+         }
+
+         remove
+         {
+            _FrameUpdating -= value;
+            if(_FrameUpdating == null)
+               CompositionTarget.Rendering -= CompositionTarget_Rendering;
+         }
+      }
+
+      static void CompositionTarget_Rendering(object sender, EventArgs e)
+      {
+         RenderingEventArgs args = e as RenderingEventArgs;
+         if(args.RenderingTime != _last)
+         {
+            _last = args.RenderingTime;
+            _FrameUpdating(sender, args);
+         }
+      }
+   }
+}
