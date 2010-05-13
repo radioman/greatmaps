81a931038e81c06333634e4d3966152914fe46ed
radioman <none@none>
2010-05-13 23:55:33 +0300

    updated versions and edited build scripts

--- Build/release-linux.bat
+++ Build/release-linux.bat
@@ -1,8 +1,9 @@
 
 "C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\Demo.WindowsForms.exe
 "C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\GMap.NET.Core.dll
-"C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\GMap.NET.WindowsForms.dll
 "C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\GMap.NET.Core.xml
+"C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\GMap.NET.Core.XmlSerializers.dll
+"C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\GMap.NET.WindowsForms.dll
 "C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Release\GMap.NET.WindowsForms.xml
 "C:\Program Files\7-Zip\7z.exe" a GMap.NET.WindowsForms.Mono.zip .\Info.txt
 
