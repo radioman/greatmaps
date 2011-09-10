2620e87483fbca47fad41f5af0d3c97ec7478cd2
radioman <none@none>
2011-09-11 00:48:30 +0300

    GMap.NET.Core: added 12 zoom layer for LithuaniaMapProvider

--- GMap.NET.Core/GMap.NET.Projections/MapsLTProjection.cs
+++ GMap.NET.Core/GMap.NET.Projections/MapsLTProjection.cs
@@ -435,14 +435,9 @@ namespace GMap.NET.Projections
       }
 
       #region -- levels info --
-      //dojo.io.script.jsonp_dojoIoScript1._jsonpCallback({"serviceDescription":"",
-      //      "mapName":"map","description":"","copyrightText":"",
-      //      "layers":[{"id":0,"name":"lietuva_MAPSLT_DB_GB200_01_P_APSK",
-      //      "parentLayerId":-1,"defaultVisibility":true,"subLayerIds":null}],
-      //      "spatialReference":{"wkid":2600},"singleFusedMapCache":true,
-      //      "tileInfo":{"rows":256,"cols":256,"dpi":96,"format":"PNG8","compressionQuality":0,
-      //      "origin":{"x":-5122000,"y":10000100},"spatialReference":{"wkid":2600},
-      //      "lods":[
+      //"tileInfo":{"rows":256,"cols":256,"dpi":96,"format":"PNG32","compressionQuality":0,
+      //"origin":{"x":-5122000,"y":10000100},"spatialReference":{"wkid":3346},
+
       //{"level":0,"resolution":1587.50317500635,"scale":6000000},
       //{"level":1,"resolution":793.751587503175,"scale":3000000},
       //{"level":2,"resolution":529.167725002117,"scale":2000000},
@@ -454,17 +449,14 @@ namespace GMap.NET.Projections
       //{"level":8,"resolution":6.61459656252646,"scale":25000},
       //{"level":9,"resolution":2.64583862501058,"scale":10000},
       //{"level":10,"resolution":1.32291931250529,"scale":5000},
-      //{"level":11,"resolution":0.529167725002117,"scale":2000}]},
+      //{"level":11,"resolution":0.529167725002117,"scale":2000},
+      //{"level":12,"resolution":0.264583862501058,"scale":1000}]},
 
-      //"initialExtent":{"xmin":-42686.481789127,"ymin":5746881.05416859,
-      //                 "xmax":1029393.00578913,"ymax":6484161.30783141,
-      //"spatialReference":{"wkid":2600}},
-      //"fullExtent":{"xmin":-42686.481789127,"ymin":5746881.05416859,
-      //              "xmax":1029393.00578913,"ymax":6484161.30783141,
-      //"spatialReference":{"wkid":2600}},"units":"esriMeters",
+      //"initialExtent":{"xmin":-412335.466179159,"ymin":5288235.83180987,
+      //                 "xmax":1417335.46617916,"ymax":6965767.82449726,
 
-      //"supportedImageFormatTypes":"PNG24,PNG,JPG,DIB,TIFF,EMF,PS,PDF,GIF,SVG,SVGZ,AI",
-      //"documentInfo":{"Title":"mapslt_minimal","Author":"gstanevicius","Comments":"","Subject":"","Category":"","Keywords":""}}); 
+      //"fullExtent":{"xmin":-45000,"ymin":5750000,
+      //              "xmax":1050000,"ymax":6500000, units":"esriMeters"
       #endregion
 
       public static double GetTileMatrixResolution(int zoom)
@@ -543,6 +535,12 @@ namespace GMap.NET.Projections
             case 11:
             {
                ret = 0.529167725002117;
+            }
+            break;
+
+            case 12:
+            {
+               ret = 0.264583862501058;
             }
             break;
             #endregion
@@ -634,6 +632,13 @@ namespace GMap.NET.Projections
                ret = new GSize(39858, 27514);
             }
             break;
+
+            case 12:
+            {
+               ret = new GSize(79716, 27514);
+            }
+            break;
+            
             #endregion
          }
 
@@ -718,6 +723,13 @@ namespace GMap.NET.Projections
                ret = new GSize(42972, 29831);
             }
             break;
+
+            case 12:
+            {
+               ret = new GSize(85944, 59662);
+            }
+            break;
+
             #endregion
          }
 
