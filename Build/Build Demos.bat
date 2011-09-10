fd06559ec3366c317ae74ebe6991b770878cfd77
radioman <none@none>
2011-09-10 17:53:24 +0300

    Added MSBuild script for fast building without using visual studio

---
+++ Build/Build Demos.bat
@@ -0,0 +1,18 @@
+set netver=v4.0.30319
+rem set netver=v3.5
+
+set TargetFrameworkVersion4=v4.0
+set TargetFrameworkVersion3=v3.5
+set TargetFrameworkVersion2=v2.0
+
+set msbuildexe=%WINDIR%\Microsoft.NET\Framework\%netver%\msbuild.exe
+
+mkdir .\Release
+ 
+del /q /s .\Release\*.*
+
+%msbuildexe% /nologo /p:WarningLevel=0;Optimize=True;Platform=AnyCPU;TargetFrameworkVersion=%TargetFrameworkVersion2% /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\Demo.WindowsForms\Demo.WindowsForms.csproj
+
+%msbuildexe% /nologo /p:WarningLevel=0;Optimize=True;Platform=AnyCPU;TargetFrameworkVersion=%TargetFrameworkVersion3% /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\Demo.WindowsPresentation\Demo.WindowsPresentation.csproj
+
+pause
