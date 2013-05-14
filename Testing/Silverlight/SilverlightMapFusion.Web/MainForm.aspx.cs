220392667c707740ec0815f2c470a4fa42f25a1a
radioman <none@none>
2013-05-14 17:10:55 +0300

    Silverlight: added map fusion demo

---
+++ Testing/Silverlight/SilverlightMapFusion.Web/MainForm.aspx.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using GMap.NET.MapProviders;
+using GMap.NET;
+using GMap.NET.WindowsPresentation;
+using System.Net;
+using System.Diagnostics;
+
+namespace SilverlightMapFusion.Web
+{
+    public partial class MainForm : System.Web.UI.Page
+    {
+        static MainForm()
+        {
+            try
+            {
+                //GMapProvider.WebProxy = WebRequest.DefaultWebProxy;
+                // or
+                //GMapProvider.WebProxy = new WebProxy("127.0.0.1", 1080);
+                //GMapProvider.IsSocksProxy = true;
+
+                GMapProvider.TileImageProxy = WindowsPresentationImageProxy.Instance;
+
+                GMaps.Instance.EnableTileHost(8844);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("MainForm: " + ex);
+                throw;
+            }
+
+            //GMaps.Instance.DisableTileHost();
+            //GMaps.Instance.CancelTileCaching();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ Testing/Silverlight/SilverlightMapFusion/MainPage.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Windows.Controls;
+using Microsoft.Maps.MapControl;
+
+namespace SilverlightMapFusion
+{
+    public partial class MainPage : UserControl
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class CustomTileSource : TileSource
+    {
+        readonly string UrlFormat = "http://localhost:8844/{0}/{1}/{2}/{3}";
+        readonly int DbId = 1492776782; //GMapProviders.OpenStreetMap.DbId;
+
+        // keep in mind that bing only supports mercator based maps
+
+        public override Uri GetUri(int x, int y, int zoomLevel)
+        {
+            return new Uri(string.Format(UrlFormat, DbId, zoomLevel, x, y));
+        }
+    }
+
+    public class CustomTileLayer : MapTileLayer
+    {
+        public CustomTileLayer()
+        {
+            this.TileSources.Add(new CustomTileSource());
+        }
+    }
+}
