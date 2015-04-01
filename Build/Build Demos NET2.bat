dcc77692900c1253d758f7193296bce588fd66e3
radioman <email@radioman.lt>
2015-04-01 17:54:12 +0300

    Build: updated scripts

--- Build/Build Demos NET2.bat
+++ Build/Build Demos NET2.bat
@@ -1,7 +1,7 @@
 
-rem set msbuildexe2=%WINDIR%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe
-set msbuildexe3=%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild.exe
-rem set msbuildexe=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
+rem set msbuildexe=%WINDIR%\Microsoft.NET\Framework\v2.0.50727\msbuild.exe
+rem set msbuildexe=%WINDIR%\Microsoft.NET\Framework\v3.5\msbuild.exe
+set msbuildexe=%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe
 
 set builddir2=Release-NETv2.0
 mkdir .\%builddir2% 
@@ -11,9 +11,9 @@ set builddir3=Release-NETv3.5
 mkdir .\%builddir3% 
 del /q /s .\%builddir3%\*.*
 
-%msbuildexe3% /nologo /p:WarningLevel=0;Optimize=True;Platform=AnyCPU;TargetFrameworkVersion=v2.0 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\Demo.WindowsForms\Demo.WindowsForms.csproj
+%msbuildexe% /fl /flp:LogFile=build.log;Append;errorsonly /nologo /p:WarningLevel=0;Optimize=True;Platform=AnyCPU;TargetFrameworkVersion=v2.0 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\Demo.WindowsForms\Demo.WindowsForms.csproj
 
-%msbuildexe3% /nologo /p:WarningLevel=0;Optimize=True;Platform=AnyCPU;TargetFrameworkVersion=v3.5 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\Demo.WindowsPresentation\Demo.WindowsPresentation.csproj
+%msbuildexe% /fl /flp:LogFile=build.log;Append;errorsonly /nologo /p:WarningLevel=0;Optimize=True;Platform=AnyCPU;TargetFrameworkVersion=v3.5 /clp:Verbosity=m; /t:Rebuild /p:Configuration=Release ..\Demo.WindowsPresentation\Demo.WindowsPresentation.csproj
 
 del /q /s .\%builddir2%\*.application
 del /q /s .\%builddir2%\*.exe.manifest
