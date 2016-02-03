0ce8b41ad4dcf5f400953d7ba6ccc765b5e08701
Vadim Galaktionov <gala-vadim@narod.ru>
2016-02-03 11:32:45 +0300

    Change format to Tab size 3

--- Demo.WindowsPresentation/Source/Map.cs
+++ Demo.WindowsPresentation/Source/Map.cs
@@ -6,40 +6,40 @@ using System.Diagnostics;
 
 namespace Demo.WindowsPresentation
 {
-    /// <summary>
-    /// The custom map of GMapControl 
-    /// </summary>
-    public class Map : GMapControl
-    {
-        public long ElapsedMilliseconds;
+   /// <summary>
+   /// The custom map of GMapControl 
+   /// </summary>
+   public class Map : GMapControl
+   {
+      public long ElapsedMilliseconds;
 
 #if DEBUG
 
-        private int counter;
-        readonly Typeface tf = new Typeface("GenericSansSerif");
-        readonly System.Windows.FlowDirection fd = new System.Windows.FlowDirection();
-        private readonly Stopwatch stopwatch = new Stopwatch();
+      private int counter;
+      readonly Typeface tf = new Typeface("GenericSansSerif");
+      readonly System.Windows.FlowDirection fd = new System.Windows.FlowDirection();
+      private readonly Stopwatch stopwatch = new Stopwatch();
 
-        /// <summary>
-        /// any custom drawing here
-        /// </summary>
-        /// <param name="drawingContext"></param>
-        protected override void OnRender(DrawingContext drawingContext)
-        {
-            stopwatch.Reset();
-            stopwatch.Start();
+      /// <summary>
+      /// any custom drawing here
+      /// </summary>
+      /// <param name="drawingContext"></param>
+      protected override void OnRender(DrawingContext drawingContext)
+      {
+         stopwatch.Reset();
+         stopwatch.Start();
 
-            base.OnRender(drawingContext);
-            stopwatch.Stop();
+         base.OnRender(drawingContext);
+         stopwatch.Stop();
 
-            FormattedText text =
-                new FormattedText(
-                    string.Format(CultureInfo.InvariantCulture, "{0:0.0}", Zoom) + "z, " + MapProvider + ", refresh: " +
-                    counter++ + ", load: " + ElapsedMilliseconds + "ms, render: " + stopwatch.ElapsedMilliseconds + "ms",
-                    CultureInfo.InvariantCulture, fd, tf, 20, Brushes.Blue);
-            drawingContext.DrawText(text, new Point(text.Height, text.Height));
-            text = null;
-        }
+         FormattedText text =
+            new FormattedText(
+               string.Format(CultureInfo.InvariantCulture, "{0:0.0}", Zoom) + "z, " + MapProvider + ", refresh: " +
+               counter++ + ", load: " + ElapsedMilliseconds + "ms, render: " + stopwatch.ElapsedMilliseconds + "ms",
+               CultureInfo.InvariantCulture, fd, tf, 20, Brushes.Blue);
+         drawingContext.DrawText(text, new Point(text.Height, text.Height));
+         text = null;
+      }
 #endif
-    }
+   }
 }
