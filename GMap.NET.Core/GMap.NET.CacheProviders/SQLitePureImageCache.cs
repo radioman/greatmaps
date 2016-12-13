9f7b98bef58db4cec7ffa5339467aa488c3c5800
radioman <email@radioman.lt>
2016-12-14 00:59:25 +0200

    GMap.NET.Core: update SQLiteVersion 103/3.14.2 for .net4

--- GMap.NET.Core/GMap.NET.CacheProviders/SQLitePureImageCache.cs
+++ GMap.NET.Core/GMap.NET.CacheProviders/SQLitePureImageCache.cs
@@ -47,7 +47,7 @@ namespace GMap.NET.CacheProviders
             }    
 
             string dllDir = appDataDir + "DllCache" + Path.DirectorySeparatorChar;
-            string dll = dllDir + "SQLite_v98_NET" + Environment.Version.Major + "_" + (IntPtr.Size == 8 ? "x64" : "x86") + Path.DirectorySeparatorChar + "System.Data.SQLite.DLL";
+            string dll = dllDir + "SQLite_v103_NET" + Environment.Version.Major + "_" + (IntPtr.Size == 8 ? "x64" : "x86") + Path.DirectorySeparatorChar + "System.Data.SQLite.DLL";
             if(!File.Exists(dll))
             {
                string dir = Path.GetDirectoryName(dll);
