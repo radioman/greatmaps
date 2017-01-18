068c5f5c596dea511e78662bcc1535022ecf647b
Jurgen De Leon Rodriguez <judero01@gmail.com>
2017-01-18 09:29:54 -0500

    Nueva estructura para deserializar JSON del api Google Maps

---
+++ GMap.NET.Core/GMap.NET.MapProviders/Google/GoogleStructureMapProvider.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GMap.NET.MapProviders
+{
+    #region Geocode
+
+    public class StrucGeocode
+    {
+        public List<Result> results { get; set; }
+        public string status { get; set; }
+    }
+
+    public class AddressComponent
+    {
+        public string long_name { get; set; }
+        public string short_name { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class Northeast
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Southwest
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Bounds
+    {
+        public Northeast northeast { get; set; }
+        public Southwest southwest { get; set; }
+    }
+
+    public class Location
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Northeast2
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Southwest2
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Viewport
+    {
+        public Northeast2 northeast { get; set; }
+        public Southwest2 southwest { get; set; }
+    }
+
+    public class Geometry
+    {
+        public Bounds bounds { get; set; }
+        public Location location { get; set; }
+        public string location_type { get; set; }
+        public Viewport viewport { get; set; }
+    }
+
+    public class Result
+    {
+        public List<AddressComponent> address_components { get; set; }
+        public string formatted_address { get; set; }
+        public Geometry geometry { get; set; }
+        public string place_id { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    #endregion
+
+    #region Direction
+
+    public class StrucDirection
+    {
+        public List<GeocodedWaypoint> geocoded_waypoints { get; set; }
+        public List<Route> routes { get; set; }
+        public string status { get; set; }
+    }
+
+    public class GeocodedWaypoint
+    {
+        public string geocoder_status { get; set; }
+        public string place_id { get; set; }
+        public List<string> types { get; set; }
+    }
+
+    public class Distance
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Duration
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class EndLocation
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class StartLocation
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Distance2
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class Duration2
+    {
+        public string text { get; set; }
+        public int value { get; set; }
+    }
+
+    public class EndLocation2
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Polyline
+    {
+        public string points { get; set; }
+    }
+
+    public class StartLocation2
+    {
+        public double lat { get; set; }
+        public double lng { get; set; }
+    }
+
+    public class Step
+    {
+        public Distance2 distance { get; set; }
+        public Duration2 duration { get; set; }
+        public EndLocation2 end_location { get; set; }
+        public string html_instructions { get; set; }
+        public Polyline polyline { get; set; }
+        public StartLocation2 start_location { get; set; }
+        public string travel_mode { get; set; }
+        public string maneuver { get; set; }
+    }
+
+    public class Leg
+    {
+        public Distance distance { get; set; }
+        public Duration duration { get; set; }
+        public string end_address { get; set; }
+        public EndLocation end_location { get; set; }
+        public string start_address { get; set; }
+        public StartLocation start_location { get; set; }
+        public List<Step> steps { get; set; }
+        public List<object> traffic_speed_entry { get; set; }
+        public List<object> via_waypoint { get; set; }
+    }
+
+    public class OverviewPolyline
+    {
+        public string points { get; set; }
+    }
+
+    public class Route
+    {
+        public Bounds bounds { get; set; }
+        public string copyrights { get; set; }
+        public List<Leg> legs { get; set; }
+        public OverviewPolyline overview_polyline { get; set; }
+        public string summary { get; set; }
+        public List<object> warnings { get; set; }
+        public List<object> waypoint_order { get; set; }
+    }
+
+    #endregion
+
+    #region Rute
+
+    public class StrucRute
+    {
+        public List<GeocodedWaypoint> geocoded_waypoints { get; set; }
+        public List<Route> routes { get; set; }
+        public string status { get; set; }
+    }
+
+    #endregion
+
+}
+
