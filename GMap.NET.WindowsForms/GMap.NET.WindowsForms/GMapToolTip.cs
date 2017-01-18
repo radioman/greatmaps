edcda35e4e55cb942b83a76d52ecac623499d747
Jurgen De Leon Rodriguez <judero01@gmail.com>
2017-01-18 09:31:07 -0500

    Nueva diseño del tooltip de marcadores

--- GMap.NET.WindowsForms/GMap.NET.WindowsForms/GMapToolTip.cs
+++ GMap.NET.WindowsForms/GMap.NET.WindowsForms/GMapToolTip.cs
@@ -42,19 +42,20 @@ using System.Collections;
       public readonly StringFormat Format = DefaultFormat;
 
 #if !PocketPC
-      public static readonly Font DefaultFont = new Font(FontFamily.GenericSansSerif, 14, FontStyle.Bold, GraphicsUnit.Pixel);
+      public static readonly Font DefaultFont = new Font(FontFamily.GenericSansSerif, 11, FontStyle.Regular, GraphicsUnit.Pixel);
+      public static readonly Font TitleFont = new Font(FontFamily.GenericSansSerif, 11, FontStyle.Bold, GraphicsUnit.Pixel);
 #else
       public static readonly Font DefaultFont = new Font(FontFamily.GenericSansSerif, 6, FontStyle.Bold);
 #endif
 
-      /// <summary>
-      /// font
-      /// </summary>
-      [NonSerialized]
+        /// <summary>
+        /// font
+        /// </summary>
+        [NonSerialized]
       public Font Font = DefaultFont;
 
 #if !PocketPC
-      public static readonly Pen DefaultStroke = new Pen(Color.FromArgb(140, Color.MidnightBlue));
+      public static readonly Pen DefaultStroke = new Pen(Color.FromArgb(140, Color.Black));
 #else
       public static readonly Pen DefaultStroke = new Pen(Color.MidnightBlue);
 #endif
@@ -66,7 +67,7 @@ using System.Collections;
       public Pen Stroke = DefaultStroke;
 
 #if !PocketPC
-      public static readonly Brush DefaultFill = new SolidBrush(Color.FromArgb(222, Color.AliceBlue));
+      public static readonly Brush DefaultFill = new SolidBrush(Color.FromArgb(222, Color.White));
 #else
       public static readonly Brush DefaultFill = new System.Drawing.SolidBrush(Color.AliceBlue);
 #endif
@@ -77,7 +78,7 @@ using System.Collections;
       [NonSerialized]
       public Brush Fill = DefaultFill;
 
-      public static readonly Brush DefaultForeground = new SolidBrush(Color.Navy);
+      public static readonly Brush DefaultForeground = new SolidBrush(Color.DimGray);
 
       /// <summary>
       /// text foreground
@@ -88,11 +89,11 @@ using System.Collections;
       /// <summary>
       /// text padding
       /// </summary>
-      public Size TextPadding = new Size(10, 10);
+      public Size TextPadding = new Size(20, 21);
 
       static GMapToolTip()
       {
-          DefaultStroke.Width = 2;
+          DefaultStroke.Width = 1;
 
 #if !PocketPC
           DefaultStroke.LineJoin = LineJoin.Round;
@@ -100,9 +101,9 @@ using System.Collections;
 #endif
 
 #if !PocketPC
-          DefaultFormat.LineAlignment = StringAlignment.Center;
+          DefaultFormat.LineAlignment = StringAlignment.Near;
 #endif
-          DefaultFormat.Alignment = StringAlignment.Center;
+          DefaultFormat.Alignment = StringAlignment.Near;
       }   
 
       public GMapToolTip(GMapMarker marker)
@@ -114,19 +115,21 @@ using System.Collections;
       public virtual void OnRender(Graphics g)
       {
          System.Drawing.Size st = g.MeasureString(Marker.ToolTipText, Font).ToSize();
-         System.Drawing.Rectangle rect = new System.Drawing.Rectangle(Marker.ToolTipPosition.X, Marker.ToolTipPosition.Y - st.Height, st.Width + TextPadding.Width, st.Height + TextPadding.Height);
+         System.Drawing.RectangleF  rect = new System.Drawing.Rectangle(Marker.ToolTipPosition.X , Marker.ToolTipPosition.Y - st.Height, st.Width + TextPadding.Width, st.Height + TextPadding.Height);
+         System.Drawing.RectangleF rectText = new System.Drawing.Rectangle(Marker.ToolTipPosition.X , Marker.ToolTipPosition.Y  - st.Height, st.Width + TextPadding.Width, st.Height + TextPadding.Height);
          rect.Offset(Offset.X, Offset.Y);
-
+         rectText.Offset(Offset.X + 7, Offset.Y + 7);
          g.DrawLine(Stroke, Marker.ToolTipPosition.X, Marker.ToolTipPosition.Y, rect.X, rect.Y + rect.Height / 2);
-
          g.FillRectangle(Fill, rect);
-         g.DrawRectangle(Stroke, rect);
+         //g.DrawRectangle(Stroke, rect);
 
 #if PocketPC
          rect.Offset(0, (rect.Height - st.Height) / 2);
-#endif
-
-         g.DrawString(Marker.ToolTipText, Font, Foreground, rect, Format);
+#endif            
+         DrawRoundRectangle(g, Stroke, rect.X, rect.Y, rect.Width, rect.Height, 8);
+         //g.DrawString(Marker.ToolTipText, Font, Foreground, rectText, Format);
+         WriteString(g, Marker.ToolTipText, rectText);
+         g.Flush();
       }
 
 #if !PocketPC
@@ -172,6 +175,55 @@ using System.Collections;
          }
       }
 
-      #endregion
-   }
+        #endregion
+
+        public void DrawRoundRectangle(Graphics g, Pen pen, float h, float v, float width, float height, float radius)
+        {
+            using (GraphicsPath gp = new GraphicsPath())
+            {
+                gp.AddLine(h + radius, v, h + width - (radius * 2), v);
+                gp.AddArc(h + width - (radius * 2), v, radius * 2, radius * 2, 270, 90);
+                gp.AddLine(h + width, v + radius, h + width, v + height - (radius * 2));
+                gp.AddArc(h + width - (radius * 2), v + height - (radius * 2), radius * 2, radius * 2, 0, 90); // Corner
+                gp.AddLine(h + width - (radius * 2), v + height, h + radius, v + height);
+                gp.AddArc(h, v + height - (radius * 2), radius * 2, radius * 2, 90, 90);
+                gp.AddLine(h, v + height - (radius * 2), h, v + radius);
+                gp.AddArc(h, v, radius * 2, radius * 2, 180, 90);
+
+                gp.CloseFigure();
+
+                g.FillPath(Fill, gp);
+                g.DrawPath(pen, gp);
+            }
+        }
+
+        private void WriteString(Graphics g, string text, RectangleF rectText)
+        {
+            string[] vec1 = text.Split('\n');
+            for(int i=0; i<vec1.Length; i++)
+            {
+                if (vec1[i] != "")
+                {
+                    string[] vec2 = vec1[i].Split('|');
+                    if (vec2.Length > 0)
+                    {
+                        Size st = g.MeasureString(vec2[0], TitleFont).ToSize();
+                        g.DrawString(String.Format("{0}", vec2[0]), TitleFont, Foreground, rectText, Format);
+                        rectText.X += st.Width + 2;
+                        if (vec2.Length > 1)
+                        {
+                            g.DrawString(vec2[1], Font, Foreground, rectText, Format);
+                        }
+                        rectText.X -= st.Width + 2;
+                        rectText.Y += st.Height;                                             
+                    }
+                }
+                else
+                {                   
+                    Size st = g.MeasureString("\n", TitleFont).ToSize();
+                    rectText.Y += st.Height;
+                }                           
+            }
+        }
+    }
 }
